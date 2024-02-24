7a2a997a9965d2b30e386255610c300f7c3076bf
Nolan Dugué <nolan.dugue@gmail.com>
2024-02-24 13:33:14 +0100

    pierre feuille ciseaux

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return(
+    <>
+
+      <div>
+        <h1>oui</h1>
+        <p>pourquoi ça marche pas</p>
+      </div>
+      <div>
+        
+      </div>
+
+    </>
+  )
+}
+
+export default App
