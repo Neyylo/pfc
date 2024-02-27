5d498f2accadce60bfbc75254a18b8729e5375d8
Nolan Dugué <nolan.dugue@gmail.com>
2024-02-27 22:16:49 +0100

    un peu avancé

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,45 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const choices = ['paper','rock','cisor'];
+
+  const game = () => {
+    const [playerchoice, setplayerchoice] = useState(null);
+    const [computerchoice, setcomputerchoicechoice] = useState(null);
+    const [result, setresultchoice] = useState(null);
+  };
+
+  const resetgame = () => {
+    setplayerchoice(null);
+    setcomputerchoicechoice(null);
+    setresultchoice(null);
+  };
+
+  const getRandomChoice = () => {
+    return choices[Math.floor(Math.random() * choices.length)];
+  };
+
+  const determineWinner = (player, computer) => {
+    if (player === computer) return 'Égalité !';
+    if ((player === 'pierre' && computer === 'ciseaux') ||
+        (player === 'feuille' && computer === 'pierre') ||
+        (player === 'ciseaux' && computer === 'feuille')) {
+      return 'Gagné !';
+    } else {
+      return 'Perdu !';
+    }
+  };
+  
+
 
   return(
     <>
 
       <div>
-        <h1>oui</h1>
-        <p>pourquoi ça marche pas</p>
+        <h1>Rock Paper Cisor</h1>
       </div>
       <div>
-        
+        <button onClick={}></button>
       </div>
 
     </>
